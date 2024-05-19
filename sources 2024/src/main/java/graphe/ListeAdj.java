f2d5d180b3f58469ab45f68b022076a6c9be0b63
BaZz Ychnightder <pierreychnightder561@gmail.com>
2024-05-19 13:32:36 +0200

    a

--- sources 2024/src/main/java/graphe/ListeAdj.java
+++ sources 2024/src/main/java/graphe/ListeAdj.java
@@ -5,67 +5,39 @@ import java.util.List;
 
 public class ListeAdj extends Graphe {
 
-    GrapheLArcs g;
     GrapheLArcs.Arc arc ;
     private int nbSommet;
     int tailleSommet;
 
-    public ListeAdj( GrapheLArcs g){
-        this.g = g;
-        this.nbSommet = g.gettaille();
-        this.tailleSommet  = g.gettaillelistesommet() ;
+    public ListeAdj(){
+
 
     }
 
-    public String toString(){
-        StringBuilder sb = new  StringBuilder("{");
-        sb.append(nbSommet);
-        sb.append(",");
-        for (int i = 0; i < tailleSommet;  ++i) {
-            sb.append(g.getSommets().get(i));// ici on a eu faux quand on a mit indexof ça retourne l'index et non la valeur
-            String sommet = g.getSommets().get(i);
-            sb.append(",");
-            sb.append(g.gettaillesucc(sommet));
-            sb.append(",");
-            for (int j = 0; j < g.gettaillesucc(sommet); ++j) {
-                sb.append(g.getSucc(sommet).get(j));
-            }
-
-            if( i >=  tailleSommet-1 ){
-                sb.append("");
-            }
-            else {
-                sb.append(",");
-            }
-        }
-        sb.append("}");
-        return sb.toString();
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//    public String toString(){
+//        StringBuilder sb = new  StringBuilder("{");
+//        sb.append(nbSommet);
+//        sb.append(",");
+//        for (int i = 0; i < tailleSommet;  ++i) {
+//            sb.append(g.getSommets().get(i));// ici on a eu faux quand on a mit indexof ça retourne l'index et non la valeur
+//            String sommet = g.getSommets().get(i);
+//            sb.append(",");
+//            sb.append(g.gettaillesucc(sommet));
+//            sb.append(",");
+//            for (int j = 0; j < g.gettaillesucc(sommet); ++j) {
+//                sb.append(g.getSucc(sommet).get(j));
+//            }
+//
+//            if( i >=  tailleSommet-1 ){
+//                sb.append("");
+//            }
+//            else {
+//                sb.append(",");
+//            }
+//        }
+//        sb.append("}");
+//        return sb.toString();
+//    }
 
     @Override
     public void ajouterSommet(String noeud) {}
